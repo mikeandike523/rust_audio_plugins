ed839a4eba61f34847c3475f01abe4b03e3ba6a0
Michael Sohnen <michaelsohnenacademic@gmail.com>
2025-07-10 21:40:42 -0400

    Got a webview based plugin beginning to work by piggy-backign of another's project. Only had like 3 annuerisms along the way. Anyway, Reaper seems to load the plugin fine but Juce AudioPluginHost is having issues. Will work on that more later.

--- custom_plugins/harmonic_nxo/src/lib.rs
+++ custom_plugins/harmonic_nxo/src/lib.rs
@@ -1,40 +1,60 @@
 mod ADSR;
 use ADSR::{Adsr, CurveType};
+use nih_plug_webview::*;
 
 use nih_plug::prelude::*;
-use std::{collections::{HashMap, VecDeque}, sync::Arc, num::NonZeroU32};
+use std::{
+    any::Any,
+    collections::{HashMap, VecDeque},
+    num::NonZeroU32,
+    sync::Arc,
+    thread,
+};
+
+use serde::Deserialize;
+use serde_json::json;
+
+use std::sync::atomic::{AtomicBool, Ordering};
 
 const MAX_VOICES: usize = 16;
 
+// Default ADSR constants (handled in WebView)
+const DEFAULT_ATTACK: f32 = 0.01;
+const DEFAULT_DECAY: f32 = 0.05;
+const DEFAULT_SUSTAIN: f32 = 0.7;
+const DEFAULT_RELEASE: f32 = 0.1;
+
+/// Plugin parameters: only Gain param here
 #[derive(Params)]
-struct SynthParams {
+struct PluginParams {
     #[id = "gain"]
     pub gain: FloatParam,
-
-    #[id = "attack"]
-    pub attack: FloatParam,
-    #[id = "decay"]
-    pub decay: FloatParam,
-    #[id = "sustain"]
-    pub sustain: FloatParam,
-    #[id = "release"]
-    pub release: FloatParam,
 }
 
-impl Default for SynthParams {
+impl Default for PluginParams {
     fn default() -> Self {
-        Self {
-            gain: FloatParam::new("Gain", -10.0, FloatRange::Linear { min: -30.0, max: 0.0 })
-                .with_smoother(SmoothingStyle::Linear(3.0))
-                .with_step_size(0.01)
-                .with_unit(" dB"),
-
-            attack: FloatParam::new("Attack", 0.01, FloatRange::Skewed { min: 0.001, max: 2.0, factor: 0.5 }),
-            decay: FloatParam::new("Decay", 0.05, FloatRange::Skewed { min: 0.001, max: 2.0, factor: 0.5 }),
-            sustain: FloatParam::new("Sustain", 0.7, FloatRange::Linear { min: 0.0, max: 1.0 }),
-            release: FloatParam::new("Release", 0.1, FloatRange::Skewed { min: 0.001, max: 2.0, factor: 0.5 }),
+        PluginParams {
+            gain: FloatParam::new(
+                "Gain",
+                -10.0,
+                FloatRange::Linear {
+                    min: -30.0,
+                    max: 0.0,
+                },
+            )
+            .with_smoother(SmoothingStyle::Linear(3.0))
+            .with_step_size(0.01)
+            .with_unit(" dB"),
         }
     }
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type")]
+enum Action {
+    Init,
+    SetSize { width: u32, height: u32 },
+    SetGain { value: f32 },
 }
 
 struct Voice {
@@ -47,31 +67,32 @@ struct Voice {
 }
 
 impl Voice {
-    pub fn new(sr: f32, params: &SynthParams) -> Self {
+    pub fn new(sr: f32) -> Self {
+        let mut env = Adsr::new(
+            DEFAULT_ATTACK,
+            DEFAULT_DECAY,
+            DEFAULT_SUSTAIN,
+            DEFAULT_RELEASE,
+            sr,
+            CurveType::Exponential,
+        );
+        env.set_attack_time(DEFAULT_ATTACK);
+        env.set_decay_time(DEFAULT_DECAY);
+        env.set_sustain_level(DEFAULT_SUSTAIN);
+        env.set_release_time(DEFAULT_RELEASE);
         Self {
             note_id: 0,
             freq: 0.0,
             phase: 0.0,
             sample_rate: sr,
-            env: Adsr::new(
-                params.attack.value(),
-                params.decay.value(),
-                params.sustain.value(),
-                params.release.value(),
-                sr,
-                CurveType::Exponential,
-            ),
+            env,
             start_ts: 0,
         }
     }
 
-    pub fn trigger(&mut self, note: u8, _velocity: f32, timestamp: u64, params: &SynthParams) {
+    pub fn trigger(&mut self, note: u8, _velocity: f32, timestamp: u64) {
         self.note_id = note;
         self.freq = util::midi_note_to_freq(note);
-        self.env.set_attack_time(params.attack.value());
-        self.env.set_decay_time(params.decay.value());
-        self.env.set_sustain_level(params.sustain.value());
-        self.env.set_release_time(params.release.value());
         self.env.trigger();
         self.start_ts = timestamp;
     }
@@ -91,14 +112,13 @@ impl Voice {
     pub fn is_released_and_done(&self) -> bool {
         self.env.is_finished()
     }
-
     pub fn get_amplitude(&self) -> f32 {
         self.env.get_level()
     }
 }
 
 pub struct HarmonicNxo {
-    params: Arc<SynthParams>,
+    params: Arc<PluginParams>,
     sample_rate: f32,
     voices: Vec<Voice>,
     active_voices: HashMap<u8, usize>,
@@ -109,7 +129,7 @@ pub struct HarmonicNxo {
 impl Default for HarmonicNxo {
     fn default() -> Self {
         Self {
-            params: Arc::new(SynthParams::default()),
+            params: Arc::new(PluginParams::default()),
             sample_rate: 44100.0,
             voices: Vec::new(),
             active_voices: HashMap::new(),
@@ -143,84 +163,120 @@ impl Plugin for HarmonicNxo {
         &mut self,
         _layout: &AudioIOLayout,
         config: &BufferConfig,
-        _context: &mut impl InitContext<Self>
+        _context: &mut impl InitContext<Self>,
     ) -> bool {
         self.sample_rate = config.sample_rate;
         true
     }
-fn process(
-    &mut self,
-    buffer: &mut Buffer,
-    _aux: &mut AuxiliaryBuffers,
-    context: &mut impl ProcessContext<Self>
-) -> ProcessStatus {
-    let mut events = Vec::new();
-    while let Some(evt) = context.next_event() {
-        events.push(evt);
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        let mut events = Vec::new();
+        while let Some(evt) = context.next_event() {
+            events.push(evt);
+        }
+        for (sample_id, mut channels) in buffer.iter_samples().enumerate() {
+            self.ts = self.ts.wrapping_add(1);
+            for evt in events.iter().filter(|e| e.timing() as usize == sample_id) {
+                match evt {
+                    NoteEvent::NoteOn { note, velocity, .. } => {
+                        self.garbage_collect();
+                        let idx = if self.voices.len() < MAX_VOICES {
+                            self.voices.push(Voice::new(self.sample_rate));
+                            let i = self.voices.len() - 1;
+                            self.queue.push_back(i);
+                            i
+                        } else {
+                            self.voices
+                                .iter()
+                                .enumerate()
+                                .min_by(|(_, a), (_, b)| {
+                                    a.get_amplitude().partial_cmp(&b.get_amplitude()).unwrap()
+                                })
+                                .map(|(i, _)| i)
+                                .unwrap_or(0)
+                        };
+                        self.queue.retain(|&i| i != idx);
+                        self.queue.push_back(idx);
+                        self.voices[idx].trigger(*note, *velocity, self.ts);
+                        self.active_voices.insert(*note, idx);
+                    }
+                    NoteEvent::NoteOff { note, .. } => {
+                        if let Some(&i) = self.active_voices.get(note) {
+                            self.voices[i].release();
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            let mut out_sample = 0.0;
+            for v in &mut self.voices {
+                let voice_sample = v.next_sample();
+                if voice_sample != 0.0 {
+                    let gain = util::db_to_gain_fast(self.params.gain.smoothed.next());
+                    out_sample += voice_sample * gain;
+                }
+            }
+            for s in channels.iter_mut().take(2) {
+                *s = out_sample;
+            }
+        }
+        ProcessStatus::KeepAlive
     }
 
-    for (sample_id, mut channels) in buffer.iter_samples().enumerate() {
-        self.ts = self.ts.wrapping_add(1);
-
-        for evt in events.iter().filter(|e| e.timing() as usize == sample_id) {
-            match evt {
-                NoteEvent::NoteOn { note, velocity, .. } => {
-                    self.note_garbage_collection();
-                    let idx = if self.voices.len() < MAX_VOICES {
-                        self.voices.push(Voice::new(self.sample_rate, &self.params));
-                        let i = self.voices.len() - 1;
-                        self.queue.push_back(i);
-                        i
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        let params = self.params.clone();
+        let editor = WebViewEditor::new(HTMLSource::URL("http://localhost:5173"), (640, 480))
+            .with_background_color((150, 150, 150, 255))
+            .with_developer_mode(true)
+            .with_keyboard_handler(move |event| {
+                println!("keyboard event: {event:#?}");
+                event.key == Key::Escape
+            })
+            .with_event_loop(move |ctx, setter, window| {
+                while let Ok(value) = ctx.next_event() {
+                    if let Ok(action) = serde_json::from_value(value) {
+                        match action {
+                            Action::SetGain { value } => {
+                                setter.begin_set_parameter(&params.gain);
+                                setter.set_parameter_normalized(&params.gain, value);
+                                setter.end_set_parameter(&params.gain);
+                            }
+                            Action::SetSize { width, height } => {
+                                ctx.resize(window, width, height);
+                            }
+                            Action::Init => {
+                                ctx.send_json(json!({
+                                    "type": "set_size",
+                                    "width": ctx.width.load(Ordering::Relaxed),
+                                    "height": ctx.height.load(Ordering::Relaxed)
+                                }));
+                            }
+                        }
                     } else {
-                        self.voices.iter().enumerate()
-                            .min_by(|(_, a), (_, b)| a.get_amplitude().partial_cmp(&b.get_amplitude()).unwrap())
-                            .map(|(i, _)| i)
-                            .unwrap_or(0)
-                    };
-                    self.queue.retain(|&i| i != idx);
-                    self.queue.push_back(idx);
-                    self.voices[idx].trigger(*note, *velocity, self.ts, &self.params);
-                    self.active_voices.insert(*note, idx);
-                }
-                NoteEvent::NoteOff { note, .. } => {
-                    if let Some(&i) = self.active_voices.get(note) {
-                        self.voices[i].release();
+                        panic!("Invalid action received from web UI.")
                     }
                 }
-                _ => {}
-            }
-        }
-
-        let mut out_sample = 0.0;
-        for v in &mut self.voices {
-            let voice_sample = v.next_sample();
-            if voice_sample != 0.0 {
-                out_sample += voice_sample * util::db_to_gain_fast(self.params.gain.smoothed.next());
-            }
-        }
-
-        for s in channels.iter_mut().take(2) {
-            *s = out_sample;
-        }
+            });
+        Some(Box::new(editor))
     }
-
-    ProcessStatus::KeepAlive
-}
-
 }
 
 impl HarmonicNxo {
-    fn note_garbage_collection(&mut self) {
-        self.active_voices.retain(|_, &mut i| !self.voices[i].is_released_and_done());
+    fn garbage_collect(&mut self) {
+        self.active_voices
+            .retain(|_, &mut i| !self.voices[i].is_released_and_done());
     }
 }
 
 impl Vst3Plugin for HarmonicNxo {
     const VST3_CLASS_ID: [u8; 16] = *b"WTH_Harmonic_NXO";
-    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] = &[
-        Vst3SubCategory::Instrument,
-        Vst3SubCategory::Synth,
-    ];
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Instrument, Vst3SubCategory::Synth];
 }
 
 nih_export_vst3!(HarmonicNxo);
---
+++ custom_plugins/harmonic_nxo/web-gui/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+import {Div, H1} from 'style-props-html'
+
+function App() {
+  return <Div>
+    <H1>Harmonic NXO</H1>
+  </Div>
+}
+
+export default App
